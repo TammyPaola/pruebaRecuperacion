a199951591de89f593006a83ece808ce104b18c0
TammyPaola <tpc.acosta@yavirac.edu.ec>
2021-02-04 13:58:29 -0500

    paola

---
+++ TaniaAcostaPruebaConsola.app/Program.cs
@@ -0,0 +1,216 @@
+using System;
+using System.Threading;
+
+namespace TaniaAcostaPruebaConsola.app
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      //piso 1
+      Thread pisoDepartamento1 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("piso 1:");
+      pisoDepartamento1.Start();
+      Thread departamento1 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 1:");
+      departamento1.Start();
+      Thread departamento2 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 2:");
+      departamento2.Start();
+      Thread departamento3 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 3:");
+      departamento3.Start();
+      Thread departamento4 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 4:");
+      departamento4.Start();
+      Thread departamento5 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 5:");
+      departamento5.Start();
+      Thread departamento6 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 6:");
+      departamento6.Start();
+      Thread departamento7 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 7:");
+      departamento7.Start();
+      Thread departamento8 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento  del piso 8:");
+      departamento8.Start();
+      piso1.Start();
+
+      //piso2
+
+      Thread pisoDepartamento2 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("piso 2:");
+      pisoDepartamento2.Start();
+      Thread departamento1 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 1:");
+      departamento1.Start();
+      Thread departamento2 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 2:");
+      departamento2.Start();
+      Thread departamento3 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 3:");
+      departamento3.Start();
+      Thread departamento4 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 4:");
+      departamento4.Start();
+      Thread departamento5 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 5:");
+      departamento5.Start();
+      Thread departamento6 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 6:");
+      departamento6.Start();
+      Thread departamento7 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 7:");
+      departamento7.Start();
+      Thread departamento8 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 8:");
+      departamento8.Start();
+      pisoDepartamento2.Start();
+
+      //piso3
+
+      Thread pisoDepartamento3 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("piso 3:");
+      pisoDepartamento3.Start();
+      Thread departamento1 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 1:");
+      departamento1.Start();
+      Thread departamento2 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 2:");
+      departamento2.Start();
+      Thread departamento3 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 3:");
+      departamento3.Start();
+      Thread departamento4 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 4:");
+      departamento4.Start();
+      Thread departamento5 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 5:");
+      departamento5.Start();
+      Thread departamento6 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 6:");
+      departamento6.Start();
+      Thread departamento7 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 7:");
+      departamento7.Start();
+      Thread departamento8 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 8:");
+      departamento8.Start();
+
+
+      // piso 4
+      Thread pisoDepartamento4 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("piso 4:");
+      pisoDepartamento4.Start();
+      Thread departamento1 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 1:");
+      departamento1.Start();
+      Thread departamento2 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 2:");
+      departamento2.Start();
+      Thread departamento3 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 3:");
+      departamento3.Start();
+      Thread departamento4 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 4:");
+      departamento4.Start();
+      Thread departamento5 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 5:");
+      departamento5.Start();
+      Thread departamento6 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 6:");
+      departamento6.Start();
+      Thread departamento7 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 7:");
+      departamento7.Start();
+      Thread departamento8 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 8:");
+      departamento8.Start();
+
+      //piso 5
+      Thread pisoDepartamento5 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("piso 5:");
+      pisoDepartamento5.Start();
+
+
+      Thread departamento1 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 1:");
+      departamento1.Start();
+      Thread departamento2 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 2:");
+      departamento2.Start();
+      Thread departamento3 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 3:");
+      departamento3.Start();
+      Thread departamento4 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 4:");
+      departamento4.Start();
+      Thread departamento5 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 5:");
+      departamento5.Start();
+      Thread departamento6 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 6:");
+      departamento6.Start();
+      Thread departamento7 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 7:");
+      departamento7.Start();
+      Thread departamento8 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("departamento del piso 8:");
+      departamento8.Start();
+
+      Thread pisoDepartamento6 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("piso 6:");
+      pisoDepartamento6.Start();
+      Thread pisoDepartamento7 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("piso 7:");
+      pisoDepartamento7.Start();
+      Thread pisoDepartamento8 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("piso 8:");
+      pisoDepartamento8.Start();
+      Thread pisoDepartamento9 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("piso 9:");
+      pisoDepartamento9.Start();
+      Thread pisoDepartamento10 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("piso 10:");
+      pisoDepartamento10.Start();
+      Thread pisoDepartamento11 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("piso 11:");
+      pisoDepartamento11.Start();
+      Thread pisoDepartamento12 = new Thread(NumeroArrendatarios);
+      Console.WriteLine("piso 12:");
+      pisoDepartamento12.Start();
+
+      pisoDepartamento1.Join();
+      pisoDepartamento2.Join();
+      pisoDepartamento3.Join();
+      pisoDepartamento4.Join();
+      pisoDepartamento5.Join();
+      pisoDepartamento6.Join();
+      pisoDepartamento7.Join();
+      pisoDepartamento8.Join();
+      pisoDepartamento9.Join();
+      pisoDepartamento10.Join();
+      pisoDepartamento11.Join();
+      pisoDepartamento12.Join();
+
+    }
+    public static void NumeroArrendatarios()
+    {
+      var hiloActual = Thread.CurrentThread;
+      hiloActual.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+      Console.WriteLine("el piso actual {0}: ", hiloActual.ManagedThreadId);
+      Console.WriteLine("Mi Cultura es {0} :", hiloActual.CurrentCulture);
+      var random = new Random();
+      for (int i = 0; i <= 11; i++)
+      {
+        Console.WriteLine("El pìso {0} departamento {1} numero de personas {2}", hiloActual.ManagedThreadId, i);
+        Thread.Sleep(random.Next(50, 75));
+      }
+    }
+  }
+}
+
+
+
